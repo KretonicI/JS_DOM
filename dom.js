40192fc44a119564b29e3dd692456c21e2e0cc21
office10 <office10@ucione.local>
2024-05-06 18:14:17 +0200

    dodavanje

---
+++ dom.js
@@ -0,0 +1,17 @@
+
+console.log("Visina je: " + window.innerHeight);
+console.log("Širina je: " + window.innerWidth);
+console.log("Domena je: " + window.location.hostname);
+console.log("Window location: " + window.location);
+
+
+var x =window.confirm("Ovime prihvaćate našu politiku o kolačićima");
+console.log("respone = " + x);
+if (x== true) {
+    console.log("korisnik odgovorio OK");   
+}
+else {
+    console.log("korisnik odgovorio NOK");
+    window.location = "onama.html";
+}
+window.alert("test alert");
